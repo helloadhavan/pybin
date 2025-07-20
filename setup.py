6833f8a47e8994a9bd0cf98872bdfaa2f3537895
helloadhavan <165385450+helloadhavan@users.noreply.github.com>
2025-07-20 20:27:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='bin',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='a libray for handling binary data',
+)
