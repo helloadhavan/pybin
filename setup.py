e739909d2a980ae100622c0d6c3f5ce78c41eff4
helloadhavan <165385450+helloadhavan@users.noreply.github.com>
2025-07-20 20:36:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='bin',
     version='0.1',
-    packages=find_packages(),
-    install_requires=[],
-    author='Your Name',
-    description='a libray for handling binary data',
+    packages=find_packages(),  # This will include the bin/ folder with __init__.py
+    description='A library for handling binary data',
 )
